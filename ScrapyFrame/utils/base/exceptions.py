07a97409b95854a6e8f0594e665208eade74c6d9
RayRi <673479663@qq.com>
2020-04-03 10:06:57 +0800

    feat($exceptions): Add NotSupported Exceptions
    添加不支持的异常
    
    #3

---
+++ ScrapyFrame/utils/base/exceptions.py
@@ -0,0 +1,12 @@
+#-*-coding:utf8-*-
+"""
+Descriptions:
+    The script contains some exception classes:
+    * NotSupported, some configurations can't be supported 
+
+"""
+from __future__ import absolute_import
+
+class NotSupported(Exception):
+    """Configuration Unspported"""
+    pass
