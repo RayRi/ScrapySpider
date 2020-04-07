278298146661a454d5ca6fe358669600c787874a
RayRi <673479663@qq.com>
2020-04-07 18:08:26 +0800

    feat($pipeline): Add MongoDB and MySQL Pipeline
    添加 MongoDB 和 MySQL Pipeline
    
    closed #2

--- ScrapyFrame/pipelines.py
+++ ScrapyFrame/pipelines.py
@@ -4,8 +4,51 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from ScrapyFrame.utils.base import EndsPipeline
+from ScrapyFrame.utils.base import database
+
+class MySQLPipeline(EndsPipeline):
+    """MySQL Data Item Pipeline"""
+    @classmethod
+    def from_crawler(cls, crawler):
+        settings = crawler.settings
+        cls.settings = settings
+        return cls
 
 
-class RelationshipStoragePipeline(object):
+    def open_spider(self, spider):
+        super().open_spider(self, spider)
+        self._db_type = "mysql"
+        self.conn = database.MySQLConnect(db=self._db) 
+
+
     def process_item(self, item, spider):
         return item
+
+
+    def close_spider(self, spider):
+        super().close_spider(self, spider)
+        self.conn.close()
+
+
+class MongoDBPipeline(EndsPipeline):
+    """MongoDB Pipeline"""
+    @classmethod
+    def from_crawler(cls, crawler):
+        settings = crawler.settings
+        cls.settings = settings
+        return cls
+
+
+    def open_spider(self, spider):
+        super().open_spider(self, spider)
+        self.conn = database.MongoDBConnect(db=self._db)
+
+
+    def process_item(self, item, spider):
+        return item
+
+    
+    def close_spider(self, spider):
+        super().close_spider(self, spider)
+        self.conn.close()
